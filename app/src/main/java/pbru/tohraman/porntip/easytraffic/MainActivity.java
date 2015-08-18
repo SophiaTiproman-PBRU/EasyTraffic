e6f8c07cf1da312d290538e894f62f3346e59b3b
Sophia Tiproman <tiproman@rocketmail.com>
2015-08-18 15:41:41 +0700

    have changed

--- app/src/main/java/pbru/tohraman/porntip/easytraffic/MainActivity.java
+++ app/src/main/java/pbru/tohraman/porntip/easytraffic/MainActivity.java
@@ -1,33 +1,87 @@
 package pbru.tohraman.porntip.easytraffic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    // Explicit delcare variable : inside Class but outside Method
+    // Explicit declare variable : inside Class but outside Method
     private ListView trafficListView;   // Private >> only in class can use
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Initial Widget >> link traffice listview with trafficListView variable
         initialWidget();     // Ctrl+Shift+Enter to fill in ;
 
+        //Create ListView
+        createListView();
+
+
     }   // Main Method
 
-    private void initialWidget() {
-        trafficListView = (ListView)findViewById(R.id.listView);  // Alt+Enter to cast data type
+    private void createListView() {
 
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขวา";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ออก";
+        strTitle[6] = "เข้า";
+        strTitle[7] = "ออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสูง";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "รถสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "เข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง";
+        strTitle[19] = "จำกัดความสูง";
+
+        int[] intImage = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+                R.drawable.traffic_04,R.drawable.traffic_05,R.drawable.traffic_06,
+                R.drawable.traffic_07,R.drawable.traffic_08,R.drawable.traffic_09,
+                R.drawable.traffic_10,R.drawable.traffic_11,R.drawable.traffic_12,
+                R.drawable.traffic_13,R.drawable.traffic_14,R.drawable.traffic_15,
+                R.drawable.traffic_16,R.drawable.traffic_17,R.drawable.traffic_18,
+                R.drawable.traffic_19,R.drawable.traffic_20};
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
+        //Active onClick on Listview
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Intent to detail >> link to detail
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(objIntent);
+
+            }   //event
+        });
+
+    }   //createListView
+
+    private void initialWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);  // Alt+Enter to cast data type
 
 
     }
