631644e7101ea0a56b756d85be5da11abe17d450
Sophia Tiproman <tiproman@rocketmail.com>
2015-08-19 16:21:40 +0700

    Finish Version 2

--- app/src/main/java/pbru/tohraman/porntip/easytraffic/TestActivity.java
+++ app/src/main/java/pbru/tohraman/porntip/easytraffic/TestActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -18,12 +20,158 @@ public class TestActivity extends AppCompatActivity {
     private RadioGroup choiceRadioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton,
             choice3RadioButton, choice4RadioButton;
+    private String[] questionStrings;
+    private int[] imageInts;
+
+    private int radioAnInt, intdexAnInt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        //Bind Widget
+        bindWidget();
+
+        //Radio Controller
+        radioController();
+
     }   //onCreate
+
+    public void clickAnswer(View view) {
+
+        if (radioAnInt == 0) {
+            Toast.makeText(TestActivity.this, "Answer Please", Toast.LENGTH_SHORT).show();
+        } else {
+            myModel();
+        }
+
+    }   //clickAnswer
+
+    private void myModel() {
+
+        if (intdexAnInt == 9) {
+
+            showAnswerDialog();
+
+        } else {
+
+            intdexAnInt += 1;
+
+            //Change View
+            changeView(intdexAnInt);
+            changeImage(intdexAnInt);
+            changeChoice(intdexAnInt);
+
+        }
+
+    }   //myModel
+
+    private void changeChoice(int anInt) {
+
+        //Change Choice
+        int[] intTimes = {R.array.times1, R.array.times2,R.array.times3,
+                R.array.times4,R.array.times5,R.array.times6,R.array.times7,
+                R.array.times8,R.array.times9,R.array.times10};
+
+        String[] strChoice = getResources().getStringArray(intTimes[anInt]);
+
+        choice1RadioButton.setText(strChoice[0]);
+        choice2RadioButton.setText(strChoice[1]);
+        choice3RadioButton.setText(strChoice[2]);
+        choice4RadioButton.setText(strChoice[3]);
+
+    }   //changeChoice
+
+    private void changeImage(int anInt) {
+
+        //Change Image
+        trafficImageView.setImageResource(imageInts[anInt]);
+    }   //chamgeImage
+
+    private void changeView(int anInt) {
+
+        //Change Question
+        questionTextView.setText(questionStrings[anInt]);
+
+    }   //changeView
+
+    private void showAnswerDialog() {
+
+    }   //showAnswerDialog
+
+
+    private void radioController() {
+
+        choiceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+                    case R.id.radioButton:
+                        radioAnInt = 1;
+                        break;
+                    case R.id.radioButton2:
+                        radioAnInt = 2;
+                        break;
+                    case R.id.radioButton3:
+                        radioAnInt = 3;
+                        break;
+                    case R.id.radioButton4:
+                        radioAnInt = 4;
+                        break;
+                    default:
+                        radioAnInt = 0;
+                        break;
+                }
+
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        //Setup
+        questionStrings = getResources().getStringArray(R.array.question);
+        imageInts = new int[10];
+        imageInts[0] = R.drawable.traffic_01;
+        imageInts[1] = R.drawable.traffic_02;
+        imageInts[2] = R.drawable.traffic_03;
+        imageInts[3] = R.drawable.traffic_04;
+        imageInts[4] = R.drawable.traffic_05;
+        imageInts[5] = R.drawable.traffic_06;
+        imageInts[6] = R.drawable.traffic_07;
+        imageInts[7] = R.drawable.traffic_08;
+        imageInts[8] = R.drawable.traffic_09;
+        imageInts[9] = R.drawable.traffic_10;
+
+        String[] strChoice = getResources().getStringArray(R.array.times1);
+
+        //Just Start
+        questionTextView.setText(questionStrings[0]);
+        trafficImageView.setImageResource(imageInts[0]);
+        choice1RadioButton.setText(strChoice[0]);
+        choice2RadioButton.setText(strChoice[1]);
+        choice3RadioButton.setText(strChoice[2]);
+        choice4RadioButton.setText(strChoice[3]);
+
+        super.onStart();
+    }   //onStart
+
+
+    private void bindWidget() {
+        questionTextView = (TextView) findViewById(R.id.txtQuestion);
+        trafficImageView = (ImageView) findViewById(R.id.imvTrafficTest);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
